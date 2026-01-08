cd6d8bc74cd182311de32c321f293c3b1121bbc9
Shane Loresto <c0545151@camosun.ca>
2026-01-08 15:58:05 -0800

    task 5 #4

---
+++ services/movieService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+// read op
+const getAll = () =>
+  axios.get(process.env.NEXT_PUBLIC_SERVICE_URL)
+  .then(response => response.data);
+
+const create = object =>
+  axios.post(process.env.NEXT_PUBLIC_SERVICE_URL, object)
+  .then( response => response.data );
+
+const update = ( id, object ) =>
+  axios.patch(`${process.env.NEXT_PUBLIC_SERVICE_URL}/${id}`, object)
+  .then( response => response.data );
+
+const deleteOne = id =>
+  axios.delete(`${process.env.NEXT_PUBLIC_SERVICE_URL}/${id}`)
+  .then( response => response.data );
+
+const messageService = { getAll, create, update, deleteOne };
+export default messageService;
