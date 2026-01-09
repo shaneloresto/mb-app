948bdda29106abf4ebd29210acf970542c0d2b45
Shane Loresto <c0545151@camosun.ca>
2026-01-09 11:04:21 -0800

    refactored MessageBoard

--- components/MessageBoard.jsx
+++ components/MessageBoard.jsx
@@ -1,16 +1,22 @@
+'use client'
 import { useState } from 'react';
-import FilterMessage from "./components/FilterMessage";
-import DisplayMessages from "./components/DisplayMessages";
-import Box from '@mui/material/Box';
-import { Link } from 'react-router';
-const MessageBoard = ({changePage}) => {
+import FilterMessage from "./FilterMessage";
+import DisplayMessages from "./DisplayMessages";
+import Link from 'next/link';
+
+const MessageBoard = () => {
     const [searchMessage, setSearchMessage] = useState('');
     return (
         <>
             <FilterMessage searchMessage={searchMessage} handleSearchMessageChange={event => setSearchMessage(event.target.value)}/>
-                <Box sx={{ ml: 2, mt: 2, width: 265, display: 'flex', justifyContent: 'flex-end' }}>
+                {/* <Box sx={{ ml: 2, mt: 2, width: 265, display: 'flex', justifyContent: 'flex-end' }}>
                     <Link to="/addmessage">Add Message</Link>
-                </Box>
+                </Box> */}
+                <div>
+                    <Link href="/addmessage">
+                        Add a Message
+                    </Link>
+                </div>
             <DisplayMessages searchMessage={searchMessage}/>
         </>
     );
