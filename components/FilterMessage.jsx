9e7231c86f4dde3ae7beecb7bf1d7bc049b7fbe2
Shane Loresto <c0545151@camosun.ca>
2026-01-09 11:07:52 -0800

    refactored FilterMessage

--- components/FilterMessage.jsx
+++ components/FilterMessage.jsx
@@ -1,13 +1,7 @@
-import { Box, TextField, Typography } from '@mui/material';
-
 const FilterMessage = ({searchMessage, handleSearchMessageChange}) => (
-  <Box sx={{ display: 'flex', alignItems: 'center' }}>
-    <Box sx={{ ml: 3 }}>
-      <Typography>Show only</Typography>
-      <Typography>messages containing:</Typography>
-    </Box>
-    <TextField sx={{ ml: 1 }} value={searchMessage} onChange={handleSearchMessageChange} />
-  </Box>
+  <div>
+    <span>Show only messages containing:</span>
+    <input value={searchMessage} onChange={handleSearchMessageChange} />
+  </div>
 );
-
 export default FilterMessage;
