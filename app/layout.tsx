51d8551fa13acba87ac7119763c15f381d743402
Shane Loresto <c0545151@camosun.ca>
2026-01-08 16:29:52 -0800

    wrap children in MessagesProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import MessagesProvider from "@/providers/MoviesProvider";
 export const metadata: Metadata = {
   title: "ICS 221 Message Board App",
   description: "Front-end App for ICS 221",
 };
 const RootLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <html lang="en">
-    <body>{children}</body>
-    </html>
+    <MessagesProvider>{children}</MessagesProvider>
   );
 }
 export default RootLayout;
--- context/moviesContext.js
+++ context/moviesContext.js
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
 
-const moviesContext = createContext();
+const MoviesContext = createContext();
 
-export default moviesContext;
+export default MoviesContext;
