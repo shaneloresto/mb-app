5dbbb5e6c268576dbd1bd066ebaa93bb60f0628d
Shane Loresto <c0545151@camosun.ca>
2026-01-09 11:32:39 -0800

    refactored EnterMessageForm

--- components/EnterMessageForm.jsx
+++ components/EnterMessageForm.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
-import {
-  Box,
-  Button,
-  FormControl,
-  TextField
-} from '@mui/material';
+'use client'
+import { useState, useContext } from "react";
+import MessagesContext from '../context/MessagesContext';
 
-const EnterMessageForm = ({ addMessage }) => {
+const EnterMessageForm = () => {
 
   const [message, setMessage] = useState('');
-
+  const {addMessage} = useContext(MessagesContext);
 
   const addNewMessage = event => {
     event.preventDefault();
@@ -19,22 +15,31 @@ const EnterMessageForm = ({ addMessage }) => {
 
 
   return (
-    <Box sx={{ height: 150, width: 365, m: 2, p: 2, border: '2px solid grey'}}>
+    // <Box sx={{ height: 150, width: 365, m: 2, p: 2, border: '2px solid grey'}}>
+    //   <form onSubmit={addNewMessage}>
+    //     <FormControl component="fieldset"
+    //       sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '1em' }}>
+    //       <TextField
+    //         label="Enter a Message:"
+    //         value={message}
+    //         onChange={event => setMessage(event.target.value)}
+    //       />
+    //       <Button
+    //         variant="contained"
+    //         color="primary"
+    //         type="submit" disabled={!message}>Add Message</Button>
+    //     </FormControl>
+    //   </form>
+    // </Box>
+    <div>
       <form onSubmit={addNewMessage}>
-        <FormControl component="fieldset"
-          sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '1em' }}>
-          <TextField
-            label="Enter a Message:"
-            value={message}
-            onChange={event => setMessage(event.target.value)}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            type="submit" disabled={!message}>Add Message</Button>
-        </FormControl>
+        <fieldset>
+          <legend>Enter a Message:</legend>
+          <input value={message} onChange={event => setMessage(event.target.value)} />
+          <button type="submit" disabled={!message}>Add Message</button>
+        </fieldset>
       </form>
-    </Box>
+    </div>
   );
 }
 
