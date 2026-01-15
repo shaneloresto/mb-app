b18bac62c3bbe312f12b12f201879a64c0ca6d05
Shane Loresto <c0545151@camosun.ca>
2026-01-15 12:55:59 -0800

    fixed some stuff

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => (
-    <div className="flex flex-col justify-center bg-accent w-80 h-20 px-5 mt-5">
-        <h1 className="text-xl">ICS 221 Message Board App</h1>
-        <h1>by Shane</h1>
+    <div className="flex flex-col justify-center bg-accent w-80 h-25 px-5 mt-5">
+        <h1 className="md:text-xl">ICS 221 Message Board App</h1>
+        <h1 className="md:text-xl">by Shane</h1>
     </div>
 )
 
