85dd9e087b34e6cb450c8b5bdd6b32e0cd23fa55
Shane Loresto <c0545151@camosun.ca>
2026-02-05 11:55:41 -0800

    added suspense to layout.tsx finished task11 lab5 last part

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import MessagesProvider from "@/providers/MessagesProvider";
 import Header from "../components/Header";
 import messageService from "@/services/messageService";
-
+import { Suspense } from 'react';
 export const metadata: Metadata = {
   title: "ICS 221 Message Board App",
   description: "Front-end App for ICS 221",
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 const RootLayout = ({children}: {children: React.ReactNode}) => {
   const messagesPromise = messageService.getAll();
   return (
-    <html><body><div className="flex justify-center"><Header/></div><MessagesProvider messagesPromise={messagesPromise}>{children}</MessagesProvider></body></html>
+    <html><body><div className="flex justify-center"><Header/></div><Suspense fallback={<div>Loading...</div>}><MessagesProvider messagesPromise={messagesPromise}>{children}</MessagesProvider></Suspense></body></html>
   );
 }
 export default RootLayout;
