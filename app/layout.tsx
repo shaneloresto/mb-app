52baea3a2e7272098461d17ae3bf5546375a5a2f
Shane Loresto <c0545151@camosun.ca>
2026-02-05 11:33:25 -0800

    first commit for task 11 lab 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 import MessagesProvider from "@/providers/MessagesProvider";
 import Header from "../components/Header";
+import messageService from "@/services/messageService";
 
 export const metadata: Metadata = {
   title: "ICS 221 Message Board App",
   description: "Front-end App for ICS 221",
 };
 const RootLayout = ({children}: {children: React.ReactNode}) => {
+  const messagesPromise = messageService.getAll();
   return (
     <html><body><div className="flex justify-center"><Header/></div><MessagesProvider>{children}</MessagesProvider></body></html>
   );
