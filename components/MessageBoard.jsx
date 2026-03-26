05e7388ac5fb466be5b2518b2efd1b79951ad1d8
Shane Loresto <c0545151@camosun.ca>
2026-03-26 10:48:34 -0700

    added logInUser() to collect credentials

--- components/MessageBoard.jsx
+++ components/MessageBoard.jsx
@@ -3,21 +3,33 @@ import { useState } from 'react';
 import FilterMessage from "./FilterMessage";
 import DisplayMessages from "./DisplayMessages";
 import Link from 'next/link';
+import LoginForm from "./LoginForm"
 
 const MessageBoard = () => {
     const [searchMessage, setSearchMessage] = useState('');
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const logInUser = (formData) => {
+        console.log(formData);
+        setIsAuthenticated(true);
+    };
     return (
         <div className='flex flex-col items-center'>
-            <FilterMessage searchMessage={searchMessage} handleSearchMessageChange={event => setSearchMessage(event.target.value)}/>
-                {/* <Box sx={{ ml: 2, mt: 2, width: 265, display: 'flex', justifyContent: 'flex-end' }}>
-                    <Link to="/addmessage">Add Message</Link>
-                </Box> */}
+            {isAuthenticated ? (
+                <>
+                <FilterMessage 
+                    searchMessage={searchMessage} 
+                    handleSearchMessageChange={event => setSearchMessage(event.target.value)}
+                />
                 <div className="flex justify-center items-center mt-5">
                     <Link className="btn btn-primary" href="/addmessage">
                         Add a Message
                     </Link>
                 </div>
-            <DisplayMessages searchMessage={searchMessage}/>
+                <DisplayMessages searchMessage={searchMessage}/>
+                </>
+            ) : (
+                <LoginForm logInUser={logInUser} />
+            )}
         </div>
     );
 }
