a554b9a372e87d199fe574dc31e646e858cab25a
Shane Loresto <c0545151@camosun.ca>
2026-04-02 09:15:49 -0700

    finish week 13 - lecture 1

--- providers/SocketIOProvider.jsx
+++ providers/SocketIOProvider.jsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { SocketIOContext } from '@/context/SocketIOContext';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { io } from 'socket.io-client';
+import MessagesContext from '@/context/MessagesContext';
 
 const SocketIOProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
   const [status, setStatus] = useState('waiting');
   const [error, setError] = useState(null);
+  const { messages, addMessage } = useContext(MessagesContext);
 
   useEffect( () => {
     const socket = io(process.env.NEXT_PUBLIC_SOCKET);
@@ -22,6 +24,12 @@ const SocketIOProvider = ({ children }) => {
         })
     socket.on('disconnect', () => setStatus('disconnected'));
     },[]);
+    useEffect( () => {
+        if (socket) {
+            socket.on('chat message', addMessage)
+            return () => socket.off('chat message', addMessage)
+        }
+    },[socket, messages]);
     return (
     <SocketIOContext.Provider value={{ socket, status, error }}>
         {children}
